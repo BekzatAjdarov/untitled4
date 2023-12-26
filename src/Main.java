5edb4f5448aefccd2e10d235326572bd19f93693
Aidarbek uulu Bekzat <ajdarovbeka137@gmail.com>
2023-12-26 19:34:10 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Double [] chik={2.3,4.5,-2.1,4.3,-1.4,-3.6,3.2,4.7,12.2,-32.1,23.4,54.6,-10.2,3.2,4.6};
+        Double sum=0.0;
+        for(Double Currentchik : chik){
+            sum = sum+Currentchik;
+            System.out.println(sum);
+
+        }
+            
+        }
+    }
